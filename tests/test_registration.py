0b2e221878516bfbea867b1c0fa2be1ca8fbcae5
CS-Magnat <58786650+CS-Magnat@users.noreply.github.com>
2025-12-01 10:49:53 +0800

    fix test_registration

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,27 +1,48 @@
 from playwright.sync_api import sync_playwright, expect, Page
 import pytest  # Импортируем библиотеку pytest
 
+from pages.dashboard_page import DashboardPage
+from pages.registration_page import RegistrationPage
 
 
 @pytest.mark.regression  # Добавили маркировку regression
 @pytest.mark.registration  # Добавили маркировку registration
-def test_successful_registration(chromium_page: Page):  # Теперь используем фикстуру
-    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-    email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill('user.name@gmail.com')
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
+    registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    registration_page.fill_registration_form(username="username", email='user.name@gmail.com', password='password')
+    registration_page.click_registration_button()
+    dashboard_page.check_dashboard_title_visible()
 
-    username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
-    username_input.fill('username')
 
-    password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill('password')
 
-    registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
-    registration_button.click()
 
-    dashboard_title = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_title).to_be_visible()
+
+
+
+
+
+# def test_successful_registration(chromium_page: Page):  # Теперь используем фикстуру
+#     chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+#
+#     email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
+#     email_input.fill('user.name@gmail.com')
+#
+#     username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
+#     username_input.fill('username')
+#
+#     password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
+#     password_input.fill('password')
+#
+#     registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
+#     registration_button.click()
+#
+#     dashboard_title = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
+#     expect(dashboard_title).to_be_visible()
+
+
+
+
 
     # with sync_playwright() as playwright:
     #     browser = playwright.chromium.launch(headless=False)
