6cef830475c29939dbdb2630a67f9a892d0e3020
CS-Magnat <58786650+CS-Magnat@users.noreply.github.com>
2025-12-04 23:12:51 +0800

    add methods courses_list_page

--- pages/courses_list_page.py
+++ pages/courses_list_page.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 
 from pages.base_page import BasePage
 
@@ -27,3 +27,27 @@ class CoursesListPage(BasePage):
         self.empty_view_icon = page.get_by_test_id('courses-list-empty-view-icon')
         self.empty_view_title = page.get_by_test_id('courses-list-empty-view-title-text')
         self.empty_view_description = page.get_by_test_id('courses-list-empty-view-description-text')
+
+
+
+
+        def check_visible_courses_title(self):
+            expect(self.courses_title).to_be_visible()
+            expect(self.courses_title).to_have_text('Courses')
+
+        def check_visible_empty_view(self):
+            expect(self.empty_view_icon).to_be_visible()
+
+            expect(self.empty_view_title).to_be_visible()
+            expect(self.empty_view_title).to_have_text('There is no results')
+
+            expect(self.empty_view_description).to_be_visible()
+            expect(self.empty_view_description).to_have_text(
+                'Results from the load test pipeline will be displayed here'
+            )
+
+        def check_visible_create_course_button(self):
+            expect(self.create_course_button).to_be_visible()
+
+        def click_create_course_button(self):
+            self.create_course_button.click()
