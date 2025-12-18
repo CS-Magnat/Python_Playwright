768d1da6b63896138c0bee72bf4350721a52f73e
CS-Magnat <58786650+CS-Magnat@users.noreply.github.com>
2025-12-18 11:24:57 +0800

    add test_dashboard_displaying

--- components/navigation/navbar_component.py
+++ components/navigation/navbar_component.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 
 from components.base_component import BaseComponent
 
@@ -9,3 +9,10 @@ class NavbarComponent(BaseComponent):
 
         self.app_title = page.get_by_test_id('navigation-navbar-app-title-text')
         self.welcome_title = page.get_by_test_id('navigation-navbar-welcome-title-text')
+
+    def check_visible(self, username: str):
+        expect(self.app_title).to_be_visible()
+        expect(self.app_title).to_have_text('UI Course')
+
+        expect(self.welcome_title).to_be_visible()
+        expect(self.welcome_title).to_have_text(f'Welcome, {username}!')
