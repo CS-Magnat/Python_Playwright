6f7a67b97b3e5e25b972a4a8199e1eb15ed4d983
CS-Magnat <58786650+CS-Magnat@users.noreply.github.com>
2025-12-23 21:43:31 +0800

    optimize courses_list_page.py

--- components/views/empty_view_component.py
+++ components/views/empty_view_component.py
@@ -11,14 +11,14 @@ class EmptyViewComponent(BaseComponent):
         self.title = page.get_by_test_id(f'{identifier}-empty-view-title-text')
         self.description = page.get_by_test_id(f'{identifier}-empty-view-description-text')
 
-        def check_visible(self, title: str, description: str):
-            # Проверяем видимость иконки
-            expect(self.icon).to_be_visible()
+    def check_visible(self, title: str, description: str):
+        # Проверяем видимость иконки
+        expect(self.icon).to_be_visible()
 
-            # Проверяем видимость заголовка и его текст
-            expect(self.title).to_be_visible()
-            expect(self.title).to_have_text(title)
+        # Проверяем видимость заголовка и его текст
+        expect(self.title).to_be_visible()
+        expect(self.title).to_have_text(title)
 
-            # Проверяем видимость описания и его текст
-            expect(self.description).to_be_visible()
-            expect(self.description).to_have_text(description)
+        # Проверяем видимость описания и его текст
+        expect(self.description).to_be_visible()
+        expect(self.description).to_have_text(description)
--- pages/courses_list_page.py
+++ pages/courses_list_page.py
@@ -2,12 +2,16 @@ from playwright.sync_api import Page, expect
 
 from components.navigation.navbar_component import NavbarComponent
 from components.navigation.sidebar_component import SidebarComponent
+from components.views.empty_view_component import EmptyViewComponent
 from pages.base_page import BasePage
 
 
 class CoursesListPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
+
+        # Пустой блок при отсутствии курсов
+        self.empty_view = EmptyViewComponent(page, 'courses-list')
 
         self.sidebar = SidebarComponent(page)
         self.navbar = NavbarComponent(page)
@@ -28,12 +32,6 @@ class CoursesListPage(BasePage):
         self.course_edit_menu_item = page.get_by_test_id('course-view-edit-menu-item')
         self.course_delete_menu_item = page.get_by_test_id('course-view-delete-menu-item')
 
-        # Пустой блок при отсутствии курсов
-        self.empty_view_icon = page.get_by_test_id('courses-list-empty-view-icon')
-        self.empty_view_title = page.get_by_test_id('courses-list-empty-view-title-text')
-        self.empty_view_description = page.get_by_test_id('courses-list-empty-view-description-text')
-
-
 
 
     def check_visible_courses_title(self):
@@ -41,14 +39,9 @@ class CoursesListPage(BasePage):
         expect(self.courses_title).to_have_text('Courses')
 
     def check_visible_empty_view(self):
-        expect(self.empty_view_icon).to_be_visible()
-
-        expect(self.empty_view_title).to_be_visible()
-        expect(self.empty_view_title).to_have_text('There is no results')
-
-        expect(self.empty_view_description).to_be_visible()
-        expect(self.empty_view_description).to_have_text(
-            'Results from the load test pipeline will be displayed here'
+        self.empty_view.check_visible(
+            title='There is no results',
+            description='Results from the load test pipeline will be displayed here'
         )
 
     def check_visible_create_course_button(self):
@@ -56,7 +49,6 @@ class CoursesListPage(BasePage):
 
     def click_create_course_button(self):
         self.create_course_button.click()
-
 
 
     def check_visible_course_card(
