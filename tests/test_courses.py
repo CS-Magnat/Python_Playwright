4e7b68bcd08d3c824b66ac98c3e372ccd410fe9d
CS-Magnat <58786650+CS-Magnat@users.noreply.github.com>
2025-12-30 15:18:16 +0800

    add ImageUploadWidgetComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -30,8 +30,9 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
     courses_list_page.sidebar.check_visible()
     courses_list_page.navbar.check_visible("username")
 
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
+    courses_list_page.toolbar_view.check_visible()
+    # courses_list_page.check_visible_courses_title()
+    # courses_list_page.check_visible_create_course_button()
     courses_list_page.check_visible_empty_view()
 
 
@@ -61,8 +62,9 @@ def test_create_course(chromium_page_with_state, create_course_page, courses_lis
         min_score = "10")
     create_course_page.click_create_course_button()
 
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
+    courses_list_page.toolbar_view.check_visible()
+    # courses_list_page.check_visible_courses_title()
+    # courses_list_page.check_visible_create_course_button()
     courses_list_page.course_view.check_visible(
         index=0,
         title="Playwright",
