dd32267469b3dec1249f90a57a7c686e1c08e154
CS-Magnat <58786650+CS-Magnat@users.noreply.github.com>
2026-01-04 23:43:49 +0800

    optimize create_course_page

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -44,15 +44,21 @@ def test_create_course(chromium_page_with_state, create_course_page, courses_lis
     chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
     create_course_page.check_visible_create_course_title()
     create_course_page.check_disabled_create_course_button()
-    create_course_page.check_visible_image_preview_empty_view()
-    create_course_page.check_visible_image_upload_view()
+    # create_course_page.check_visible_image_preview_empty_view()
+    # create_course_page.check_visible_image_upload_view()
+    # Остальной код без изменений
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
+
     create_course_page.check_visible_create_course_form(title="", description="", estimated_time="", max_score="0", min_score="0")
     create_course_page.check_visible_exercises_title()
     create_course_page.check_visible_create_exercise_button()
     create_course_page.check_visible_exercises_empty_view()
 
-    create_course_page.upload_preview_image("/Users/uladzimirrudnik/PycharmProjects/Python_Playwright/testdata/files/image.jpg")
-    create_course_page.check_visible_image_upload_view()
+    # create_course_page.upload_preview_image("/Users/uladzimirrudnik/PycharmProjects/Python_Playwright/testdata/files/image.jpg")
+    # create_course_page.check_visible_image_upload_view()
+    # Остальной код без изменений
+    create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
 
     create_course_page.fill_create_course_form(
         title = "Playwright",
