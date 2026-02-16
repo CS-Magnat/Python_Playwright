f6e89b784474d9255489371379025e699a4c5a84
CS-Magnat <58786650+CS-Magnat@users.noreply.github.com>
2026-02-16 22:29:09 +0500

    optimized BaseElement

--- elements/base_element.py
+++ elements/base_element.py
@@ -9,24 +9,44 @@ class BaseElement:
         self.locator = locator
 
     # Метод принимает кейворд аргументы (kwargs)
-    def get_locator(self, **kwargs) -> Locator: # объект Locator для взаимодействия с элементом
-        # Инициализирует объект локатора, подставляя динамические значения в локатор.
-        locator = self.locator.format(**kwargs)
-        # Возвращаем объект локатора
-        return self.page.get_by_test_id(locator)
+    # def get_locator(self, **kwargs) -> Locator: # объект Locator для взаимодействия с элементом
+    #     # Инициализирует объект локатора, подставляя динамические значения в локатор.
+    #     locator = self.locator.format(**kwargs)
+    #     # Возвращаем объект локатора
+    #     return self.page.get_by_test_id(locator)
 
-    def click(self, **kwargs):
-        # "Лениво" инициализируем локатор
-        locator = self.get_locator(**kwargs)
-        # Выполняем нажатие на элемент
+    def get_locator(self, nth: int = 0, **kwargs) -> Locator:
+        # Добавляем аргумент nth со значением по умолчанию 0
+        locator = self.locator.format(**kwargs)
+        return self.page.get_by_test_id(locator).nth(nth)  # Теперь выбираем элемент по индексу
+
+    def click(self, nth: int = 0, **kwargs):
+        # Добавили аргумент nth и передеаем его в get_locator
+        locator = self.get_locator(nth, **kwargs)
         locator.click()
 
-    def check_visible(self, **kwargs):
-        # Инициализируем локатор "лениво"
-        locator = self.get_locator(**kwargs)
-        # Проверяем, что элемент виден на странице
+    def check_visible(self, nth: int = 0, **kwargs):
+        # Добавили аргумент nth и передеаем его в get_locator
+        locator = self.get_locator(nth, **kwargs)
         expect(locator).to_be_visible()
 
-    def check_have_text(self, text: str, **kwargs):
-        locator = self.get_locator(**kwargs)
+    def check_have_text(self, text: str, nth: int = 0, **kwargs):
+        # Добавили аргумент nth и передеаем его в get_locator
+        locator = self.get_locator(nth, **kwargs)
         expect(locator).to_have_text(text)
+
+    # def click(self, **kwargs):
+    #     # "Лениво" инициализируем локатор
+    #     locator = self.get_locator(**kwargs)
+    #     # Выполняем нажатие на элемент
+    #     locator.click()
+    #
+    # def check_visible(self, **kwargs):
+    #     # Инициализируем локатор "лениво"
+    #     locator = self.get_locator(**kwargs)
+    #     # Проверяем, что элемент виден на странице
+    #     expect(locator).to_be_visible()
+    #
+    # def check_have_text(self, text: str, **kwargs):
+    #     locator = self.get_locator(**kwargs)
+    #     expect(locator).to_have_text(text)
