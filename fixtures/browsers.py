7df1b96d7af5f63654c554a2a7fa9c5e99a8207d
CS-Magnat <58786650+CS-Magnat@users.noreply.github.com>
2026-03-02 21:37:12 +0500

    optimized initialize_browser_state

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -2,41 +2,44 @@ import pytest  # Импортируем pytest
 from playwright.sync_api import sync_playwright, \
     Page, expect, Playwright  # Имопртируем класс страницы, будем использовать его для аннотации типов
 
+from pages.authentication.registration_page import RegistrationPage
+
 
 @pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function, то что нам нужно
 def chromium_page(playwright: Playwright) -> Page:  # Аннотируем возвращаемое фикстурой значение
     # Запускаем браузер
     browser = playwright.chromium.launch(headless=False)
-
     # Передаем страницу для использования в тесте
     yield browser.new_page()
-
     # Закрываем браузер после выполнения тестов
     browser.close()
 
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
-
     context = browser.new_context()
-
     page = context.new_page()
 
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    # Работаем с регистрационной страницей через Page Object
+    registration_page = RegistrationPage(page=page)
+    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    # page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
+    # email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    # email_input.fill('user.name@gmail.com')
 
-    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill('user.name@gmail.com')
-
-    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    username_input.fill('username')
-
-    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill('password')
-
-    registration_button = page.get_by_test_id('registration-page-registration-button')
-    registration_button.click()
+    # username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    # username_input.fill('username')
+    #
+    # password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    # password_input.fill('password')
+    registration_page.click_registration_button()
+    # registration_button = page.get_by_test_id('registration-page-registration-button')
+    # registration_button.click()
 
     context.storage_state(path="browser-state.json")
+    browser.close()
+
 
 @pytest.fixture
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
