2427db0e8878973787bff8d07122beca164d8404
CS-Magnat <58786650+CS-Magnat@users.noreply.github.com>
2026-03-06 22:15:11 +0500

    add test test_navigate_from_authorization_to_registration

--- pages/authentication/registration_page.py
+++ pages/authentication/registration_page.py
@@ -1,3 +1,5 @@
+import re
+
 from playwright.sync_api import Page, expect
 
 from components.authentication.registration_form_component import RegistrationFormComponent
@@ -35,6 +37,8 @@ class RegistrationPage(BasePage):
     # Метод для нажатия на кнопку "Registration"
     def click_registration_button(self):
         self.registration_button.click()
+        # Добавили проверку
+        self.check_current_url(re.compile(".*/#/auth/login"))
 
     # # Метод для нажатия на ссылку "Login"
     def click_login_link(self):
