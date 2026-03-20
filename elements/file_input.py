d93273e725b853af833e22a4197f3d9c57ca221d
CS-Magnat <58786650+CS-Magnat@users.noreply.github.com>
2026-03-20 22:54:47 +0500

    add type_of in FileInput / Icon / Image / Link / Text

--- elements/file_input.py
+++ elements/file_input.py
@@ -1,5 +1,5 @@
 from elements.base_element import BaseElement
-
+import allure
 
 # class FileInput(BaseElement):
 #     def set_input_files(self, file: str, **kwargs):
@@ -7,6 +7,13 @@ from elements.base_element import BaseElement
 #         locator.set_input_files(file)
 
 class FileInput(BaseElement):
+
+    @property
+    def type_of(self) -> str:  # Переопределяем свойство type_of
+        return "file input"
+
+
     def set_input_files(self, file: str, nth: int = 0, **kwargs):
-        locator = self.get_locator(nth, **kwargs)
-        locator.set_input_files(file)
+        with allure.step(f'Set file "{file}" to the {self.type_of} "{self.name}"'):  # Добавили шаг
+            locator = self.get_locator(nth, **kwargs)
+            locator.set_input_files(file)
